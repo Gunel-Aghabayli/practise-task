b53077bc47a814999e59d854270449899b13aba6
Gunel-Aghabayli <gunelagab@gmail.com>
2025-08-26 10:50:36 +0400

    First-commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CarCom from './Components/CartCom'
+
+const App = () => {
+  return (
+    <>
+      <CarCom />
+    </>
+  );
+}
+
+export default App
---
+++ src/Components/CartCom.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import data from "../data";
+import "./CartCom.css";
+
+function CarCom() {
+  return (
+    <div>
+      <h1>Cars</h1>
+      <div className="div">
+        {data.map((car, id) => (
+          <div className="carbox" key={id}>
+            <img src={car.img} alt={car.name} />
+            <h2>{car.name}</h2>
+            <p>Year: {car.year}</p>
+            <p>Price: ${car.price}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default CarCom;
